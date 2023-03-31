2ffd915a47f020eb5fefeeac1bcfc20b5f8dbdc0
jundaeyoung <jdy1787@naver.com>
2023-03-31 19:56:11 +0900

    숙소 정보 db에 담음

--- airbnb.sql
+++ airbnb.sql
@@ -3,12 +3,42 @@ use myblog;
 SELECT * FROM user;
 SELECT * FROM reservation
 WHERE user_id ='jdy1787';
-DELETE FROM reservation;
+DELETE FROM reply;
 SELECT * FROM home;
 SELECT * FROM reservation;
 SELECT * FROM reply;
+SELECT format(avg(rating),2)as rating FROM reply
+WHERE home_id='1';
  DELETE FROM reply WHERE user_id='jdy1787'AND id='44';
 
+CREATE TABLE HomeInfo(
+	title VARCHAR(200),
+	location VARCHAR(200),
+	image1 VARCHAR(200),
+	image2 VARCHAR(200),
+	image3 VARCHAR(200),
+	image4 VARCHAR(200),
+	image5 VARCHAR(200),
+	host VARCHAR(200),
+	info VARCHAR(200),
+	sideimage1 VARCHAR(200),
+	sideInfo1 VARCHAR(200),
+    sideInfo1_1 VARCHAR(200),
+	sideimage2 VARCHAR(200),
+	sideInfo2 VARCHAR(200),
+    sideInfo2_1 VARCHAR(200),
+	sideimage3 VARCHAR(200),
+	sideInfo3 VARCHAR(200),
+    sideInfo3_1 VARCHAR(200),
+    home_id int,
+	FOREIGN KEY(home_id) REFERENCES home(id)
+);
+SELECT * FROM homeinfo;
+delete from homeinfo 
+where home_id=2;
+insert into homeinfo
+values
+	('정면오션테라스ㅡ광안리불꽃축제 명당','❣ 슈퍼호스트 · 수영구, 부산, 한국','images/home5/home5_1.png','images/home5/home5_2.png','images/home5/home5_3.png','images/home5/home5_4.png','images/home5/home5_5.png','그랜드테라스 님이 호스팅하는 레지던스 전체','최대 인원 2명 · 원룸 · 침대 1개 · 욕실 1개','superhost1.png','그랜드테라스님은 슈퍼호스트입니다','슈퍼호스트는 풍부한 경험과 높은 평점을 자랑하며 게스트가 숙소에서 편안히 머무를 수 있도록 최선을 다하는 호스트입니다..','checkin.png','셀프 체크인','열쇠 보관함을 이용해 체크인하세요.','key.png','순조로운 체크인 과정','최근 숙박한 게스트 중 100%가 체크인 과정에 별점 5점을 준 숙소입니다.',5);
 CREATE TABLE user(
 	id VARCHAR(30) NOT NULL UNIQUE,
     password VARCHAR(30) NOT NULL,
@@ -16,7 +46,12 @@ CREATE TABLE user(
     email VARCHAR(30) NOT NULL
 );
 
-ALTER TABLE reply DROP CONSTRAINT user_id;
+CREATE TABLE home_info(
+	title VARCHAR(200)
+    
+);
+
+ALTER TABLE reply add rating INT;
 ALTER TABLE reply 
 ADD FOREIGN KEY(user_id) 
 REFERENCES user(id) 
@@ -41,12 +76,7 @@ CREATE TABLE home(
 );
 insert into home(price,name,day,view)
 VALUES
-	('\296,800 / 박','Ganggu-myeon, Yeongdeok-gun,한국','3월 20일~25일','바다 전망'),
-    ('\679,531 / 박','Niseko, Abuta District, Hokkaido, 일본','3월 21일~26일','산 전망'),
-    ('\483,007 / 박','南伊豆町, 静岡県, 일본','3월 23일~26일','산 전망'),
-    ('\1,012,190 / 박','시모다, 일본','3월 20일~25일','바다 전망'),
-    ('\167,639 / 박','수영구, 한국','3월 26일~5월 1일','해변 및 바다 전망'),
-    ('\71,912 / 박','여수시, 전라남도, 한국','4월 3일~4월 8일','시티 전망');
+	('\148,353 / 박','수영,부산시,한국','4월 7일~13일','바다 전망');
 
 SELECT * FROM home
 WHERE name like '%한국' ;    
